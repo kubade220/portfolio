3ec8b40fb3632e8cce8e36ed993a973e8457aab4
kubade220 <32162454+kubade220@users.noreply.github.com>
2024-09-22 14:05:48 +0200

    footer

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,16 +8,16 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Jakub Nabywaniec </span>
+            from <span className="purple"> Świdnica, Polska.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently employed as a Network Support Engineer at Tech Domain.
             <br />
             I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
             Mesra.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from Networking, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
